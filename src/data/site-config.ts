f8d4ec5004a53136818af503a714c462e856c575
Henry Valdez Vega <henryvaldezv@gmail.com>
2025-04-13 17:35:57 -0500

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,8 +40,8 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Henry Valdez',
+    subtitle: 'Blog personal',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -98,8 +98,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hola a todos, bienvenidos a mi blog personal 🖥️',
+        text: "Mi nombre es Henry Valdez. Soy estudiante del octavo ciclo de la carrera de Ingeniería de Sistemas e Informática de la Universidad Continental. He creado este blog con fines educacionales y para mostrar algunos intereses personales. 📚💻🏆",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
